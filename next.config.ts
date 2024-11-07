e35e950ba581a98d311f394c538b623e9e05949a
Priyesh <priyeshchaudhari35@gmail.com>
2024-11-07 13:09:17 +0530

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript:{
+    ignoreBuildErrors:true,
+  },
+  images: {
+    dangerouslyAllowSVG:true,
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: '*'
+      }
+    ]
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
+  experimental: {
+    after:true,
+  }
+};
+
+export default nextConfig;
